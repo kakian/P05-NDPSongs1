e5d0642448cdc399516284d5dbc0a0cdbd001a9b
kakian <hew_ka_kian@rp.edu.sg>
2021-07-30 10:58:05 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/ourndpsongs/ThirdActivity.java
@@ -0,0 +1,127 @@
+package com.myapplicationdev.android.ourndpsongs;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    EditText etID, etTitle, etSingers, etYear;
+    Button btnCancel, btnUpdate, btnDelete;
+    RatingBar ratingBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+
+        setTitle(getTitle().toString() + " ~ " + getResources().getText(R.string.title_activity_third));
+
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        btnUpdate = (Button) findViewById(R.id.btnUpdate);
+        etID = (EditText) findViewById(R.id.etID);
+        etTitle = (EditText) findViewById(R.id.etTitle);
+        etSingers = (EditText) findViewById(R.id.etSingers);
+        etYear = (EditText) findViewById(R.id.etYear);
+        ratingBar = findViewById(R.id.ratingbarStars);
+
+        Intent i = getIntent();
+        final Song currentSong = (Song) i.getSerializableExtra("song");
+
+        etID.setText(currentSong.getId()+"");
+        etTitle.setText(currentSong.getTitle());
+        etSingers.setText(currentSong.getSingers());
+        etYear.setText(currentSong.getYearReleased()+"");
+
+        ratingBar.setRating(currentSong.getStars());
+
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelper dbh = new DBHelper(ThirdActivity.this);
+                currentSong.setTitle(etTitle.getText().toString().trim());
+                currentSong.setSingers(etSingers.getText().toString().trim());
+                int year = 0;
+                try {
+                    year = Integer.valueOf(etYear.getText().toString().trim());
+                } catch (Exception e){
+                    Toast.makeText(ThirdActivity.this, "Invalid year", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                currentSong.setYearReleased(year);
+
+                //int selectedRB = rg.getCheckedRadioButtonId();
+                //RadioButton rb = (RadioButton) findViewById(selectedRB);
+                //currentSong.setStars(Integer.parseInt(rb.getText().toString()));
+                currentSong.setStars((int) ratingBar.getRating());
+                int result = dbh.updateSong(currentSong);
+                if (result>0){
+                    Toast.makeText(ThirdActivity.this, "Song updated", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(ThirdActivity.this, "Update failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(ThirdActivity.this);
+                alertBuilder.setTitle("Danger");
+                alertBuilder.setMessage("Are you sure you want to delete the song\n" + currentSong.getTitle());
+                alertBuilder.setCancelable(true);
+                alertBuilder.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        DBHelper dbh = new DBHelper(ThirdActivity.this);
+                        int result = dbh.deleteSong(currentSong.getId());
+                        if (result>0){
+                            Toast.makeText(ThirdActivity.this, "Song deleted", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } else {
+                            Toast.makeText(ThirdActivity.this, "Delete failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                alertBuilder.setPositiveButton("Cancel", null);
+                AlertDialog alertDialog = alertBuilder.create();
+                alertBuilder.show();
+            }
+        });
+
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder alertBuilder = new AlertDialog.Builder(ThirdActivity.this);
+                alertBuilder.setTitle("Danger");
+                alertBuilder.setMessage("Are you sure you want discard the changes\n");
+                alertBuilder.setCancelable(true);
+                alertBuilder.setNegativeButton("Discard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                });
+                alertBuilder.setPositiveButton("Do NOT Discard", null);
+                AlertDialog alertDialog = alertBuilder.create();
+                alertBuilder.show();
+            }
+        });
+
+    }
+
+
+}
